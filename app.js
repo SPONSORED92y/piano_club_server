a57f61e83581666e1f57db37c2b286ecf167a6de
jasonsu92y <91020458+SPONSORED92y@users.noreply.github.com>
2023-03-03 09:11:12 +0800

    basic server framework revised

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 var createError = require('http-errors');
 const express = require('express');
+const session = require("express-session");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 const compression = require("compression");
 const { body, validationResult } = require("express-validator");
 
 const app = express();
+// passport
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    return done(null, user);
+                } else {
+                    // passwords do not match!
+                    return done(null, false, { message: "Incorrect password" });
+                }
+            });
+        });
+    })
+);
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -16,14 +55,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+//route
+// app.use('/', indexRouter);
 
 app.get('/', (req, res) => {
     // res.json({});
     res.render('form')
 });
-
 
 chat_post = (req, res, next) => {
     console.log("receive post");
@@ -36,6 +74,16 @@ app.post('/', chat_post);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
+});
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(5000, () => console.log("server start on 5000"));
